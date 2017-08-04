c4bff915c7464d0924753d940074988aedc7974e
Pablo Aguado <aguadopd@hotmail.com>
2017-08-04 20:28:28 -0300

    Asistente de nivelación de cama
    Ahora se puede nivelar la cama desde la pantalla LCD. 
    Más en http://marlinfw.org/docs/gcode/G029-mbl.html
    
    Hacerlo una vez y guardarlo en EEPROM

--- Configuración/Marlin/Configuration.h
+++ Configuración/Marlin/Configuration.h
@@ -914,7 +914,7 @@
   //=================================== Mesh ==================================
   //===========================================================================
 
-  #define MESH_INSET 10          // Mesh inset margin on print area
+  #define MESH_INSET 15          // Mesh inset margin on print area
   #define GRID_MAX_POINTS_X 3    // Don't use more than 7 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
@@ -926,7 +926,7 @@
  * Use the LCD controller for bed leveling
  * Requires MESH_BED_LEVELING or PROBE_MANUALLY
  */
-//#define LCD_BED_LEVELING
+#define LCD_BED_LEVELING
 
 #if ENABLED(LCD_BED_LEVELING)
   #define MBL_Z_STEP 0.025    // Step size while manually probing Z axis.
